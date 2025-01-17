b65ed5f27d3c007e5e0869e9370051ba0ff486c2
Ana Waldila <62858607+AnaWaldila@users.noreply.github.com>
2025-01-17 13:35:22 -0300

    Add files via upload

---
+++ Dynablast - 2024.11.18 - Versao 1.1.1/main.m
@@ -0,0 +1,11 @@
+%% Initialization
+
+% Clear workspace
+clear
+clc
+
+% Add path to canvas, cross, and images folders
+addpath('Draw','Functions','GUI', 'Constructor');
+
+%% Start up app
+App_Blast;
